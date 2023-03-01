b8ce3227a26a6ad39fb3be5ad0141d0558d52b94
Marc Almond Valleza <almond@paymongo.com>
2023-03-02 03:07:15 +0800

    Implement create reservation service

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,17 @@
 class Reservation < ApplicationRecord
   belongs_to :guest
+
+  validates :code, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :nights, presence: true, numericality: true
+  validates :guests, presence: true, numericality: true
+  validates :adults, presence: true, numericality: true
+  validates :children, presence: true, numericality: true
+  validates :infants, presence: true, numericality: true
+  validates :status, presence: true
+  validates :currency, presence: true
+  validates :payout_price, presence: true
+  validates :security_price, presence: true
+  validates :total_price, presence: true
 end
--- app/services/create_reservation.rb
+++ app/services/create_reservation.rb
@@ -6,39 +6,51 @@ class CreateReservation < ApplicationService
   end
 
   def perform
-    # errors = validate
+    errors = validate
 
-    # return errors unless errors.empty?
+    return errors unless errors.empty?
 
-    process
+    process 
   end
 
   private
 
-  # Validate payload params
-  # def validate
-
-  # end
-
-  def process
-    guest = create_guest
-
-    return format_errors(guest.errors.full_messages, 400) unless guest.valid?
+  def validate
+    CreateReservationResourceValidation.new(
+      code: params[:code],
+      guest_email: params[:guest_email]
+    ).validate
   end
-
-  def create_guest
-    Guest.create(
+  
+  def process
+    guest = Guest.new(
       email: params[:guest_email],
       first_name: params[:guest_first_name],
       last_name: params[:guest_last_name],
       phone: params[:guest_phone]
     )
+
+    if !guest.valid?
+      return Reservations::ErrorFormatter.new(errors: guest.errors.full_messages, http_code: 400).to_h
+    end
+  
+    reservation = guest.build_reservation(reservation_details)
+
+    if !reservation.valid?
+      return Reservations::ErrorFormatter.new(errors: reservation.errors.full_messages, http_code: 400).to_h
+    end
+
+    # Save records
+    guest.save
+    reservation.save
+  
+    {
+      data: reservation.as_json.merge(guest: guest.as_json),
+      status: 200
+    }
   end
 
-  def format_errors(errors, http_code)
-    {
-      errors: errors,
-      status: http_code
-    }
+  def reservation_details
+    params.except(:guest_email, :guest_first_name, :guest_last_name, :guest_phone)
   end
 end
